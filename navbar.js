5b016bdca990e04d9fb2983d82244cabbc32c376
PsychoCode02 <70811512+PsychoCode02@users.noreply.github.com>
2020-10-05 23:04:49 -0400

    Add files via upload

---
+++ navbar.js
@@ -0,0 +1,6 @@
+function toggleNavbar(collapseID) {
+    document.getElementById(collapseID).classList.toggle("hidden");
+    document.getElementById(collapseID).classList.toggle("block");
+  }
+
+  
